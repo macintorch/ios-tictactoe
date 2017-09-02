e3f1024df6af6651a887f97cfb8d4ea40a0eaef9
Ainor Syahrizal <macintorch@gmail.com>
2017-09-02 20:49:20 +0800

    each button tag-ed

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func pressButton(_ sender: Any) {
+        
+        print("Button pressed")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
