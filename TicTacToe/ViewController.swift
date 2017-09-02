12c909c8de93a9f5c2e91e9d6274fbf504641fdc
Ainor Syahrizal <macintorch@gmail.com>
2017-09-03 06:38:51 +0800

    winning combinations setup

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -11,16 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     
     // 1 is noughts, 2 is crosses
+    
+    var activeGame = true
+    
     var activePlayer = 1
     
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 - empty, 1 - noughts, 2 - crosses
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     @IBAction func pressButton(_ sender: Any) {
         
         let activePosition = (sender as AnyObject).tag - 1
         
         // to avoid other user tap on a taken spot
-        if gameState[activePosition] == 0 {
+        if gameState[activePosition] == 0 && activeGame {
             
             gameState[activePosition] = activePlayer
             
@@ -33,6 +38,18 @@ class ViewController: UIViewController {
             } else {
                 (sender as AnyObject).setImage(UIImage(named: "cross.png"), for:[])
                 activePlayer = 1
+            }
+            
+            for combination in winningCombinations {
+                
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+                    
+                    // We have a winner!
+                    activeGame = false
+                    
+                    print(gameState[combination[0]])
+                    
+                }
             }
             
         }
