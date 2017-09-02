38fd57e448ae8df753dfdb3daf3507c344f6a452
Ainor Syahrizal <macintorch@gmail.com>
2017-09-02 21:05:53 +0800

    make the user take turns press button

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // 1 is noughts, 2 is crosses
+    var activePlayer = 1
 
     @IBAction func pressButton(_ sender: Any) {
+        
+        if activePlayer == 1 {
+            
+            // set image when press button
+            (sender as AnyObject).setImage(UIImage(named: "nought.png"), for:[])
+            activePlayer = 2
+            
+        } else {
+            (sender as AnyObject).setImage(UIImage(named: "cross.png"), for:[])
+            activePlayer = 1
+        }
+        
         
         // prints the button tag
         print("\((sender as AnyObject).tag) Button pressed")
